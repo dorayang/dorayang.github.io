6cd335b4caed3e0754bfed6fa351ff437991c275
dyang22 <dyang22@student.ubc.ca>
2023-11-17 17:21:15 -0500

    Front page progress

---
+++ jobs.js
@@ -0,0 +1,41 @@
+var jobs = { 
+        "dwave":
+            {"Date": "May 2022 - Aug. 2022", 
+            "Title": "Electrical Engineer Co-op", 
+            "Company": "D-Wave", 
+            "Link": "https://www.dwavesys.com/", 
+            "Blurb": "Designed on programmeable power supply units and test modules for quantum computers.", 
+            "Skills": ["Power Electronics", "UART", "STM32"]},
+
+        "pni": 
+            {"Date": "May 2021 - Dec. 2021", 
+            "Title": "Instrumentation Engineer Co-op", 
+            "Company": "Precision Nanosystems", 
+            "Link": "https://www.precisionnanosystems.com/", 
+            "Blurb": "orem ipsum dolor sit amet, consectetur adipiscing elit,", 
+            "Skills": ["Power Electronics", "UART", "STM32"]},
+        "ised":
+            {"Date": "Jan. 2020 - Apr. 2020", 
+            "Title": "Telecommunications Engineer Co-op", 
+            "Company": "Innovation, Science and Economic Development Canada", 
+            "Link": "https://tinyurl.com/3dzv34hz", 
+            "Blurb": "Contributed to the technical regulations for deploying Wi-Fi and 5G devices in Canada's 6GHz band.", 
+            "Skills": ["Power Electronics", "UART", "STM32"]},
+
+        "carobot":
+            {"Date": "Jun. 2019 - Aug 2019", 
+            "Title": "Robotics Instructor", 
+            "Company": "Carobot", 
+            "Link": "https://learn.carobot.ca/", 
+            "Blurb": "Created lessons and taught kiddos coding and robotics fundamentals (sensors, motors, controllers).", 
+            "Skills": ["Power Electronics", "UART", "STM32"]},
+
+        "lts":
+            {"Date": "Oct. 2015 - Apr. 2018", 
+            "Title": "Learn to Skate Instructor", 
+            "Company": "City of Toronto Parks and Recreation", 
+            "Link": "https://www.toronto.ca/data/parks/prd/skating/reg/learn/index.html", 
+            "Blurb": "Taught basic skills & beginner figure skating elements to young & adult skaters.", 
+            "Skills": ["Power Electronics", "UART", "STM32"]}
+}
+
