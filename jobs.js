35101287759e34d584c1d5da971394f2a3e35102
dyang22 <dyang22@student.ubc.ca>
2023-11-18 19:56:14 -0500

    Starting on projects page

--- jobs.js
+++ jobs.js
@@ -4,31 +4,31 @@ var jobs = {
             "Title": "Electrical Engineer Co-op", 
             "Company": "D-Wave", 
             "Link": "https://www.dwavesys.com/", 
-            "Blurb": "Designed on programmeable power supply units and test modules for quantum computers.", 
-            "Skills": ["Power Electronics", "UART", "STM32"]},
+            "Blurb": "Designed programmeable power supply units and test modules for quantum computers.", 
+            "Skills": ["Power Electronics", "C++", "Python", "CAD", "3D Printing", "UART", "STM32 Microcontrollers",]},
 
         "pni": 
             {"Date": "May 2021 - Dec. 2021", 
             "Title": "Instrumentation Engineer Co-op", 
             "Company": "Precision Nanosystems", 
             "Link": "https://www.precisionnanosystems.com/", 
-            "Blurb": "orem ipsum dolor sit amet, consectetur adipiscing elit,", 
-            "Skills": ["Power Electronics", "UART", "STM32"]},
+            "Blurb": "Created and executed tests evaluate system performance and guide component selection for a commercial nanomedicine formulation machine.", 
+            "Skills": ["MATLAB", "Python", "Sensors", "Oscilloscopes", "PLCs", "Data Acquisition", "Labview"]},
         "ised":
             {"Date": "Jan. 2020 - Apr. 2020", 
             "Title": "Telecommunications Engineer Co-op", 
             "Company": "Innovation, Science and Economic Development Canada", 
             "Link": "https://tinyurl.com/3dzv34hz", 
             "Blurb": "Contributed to the technical regulations for deploying Wi-Fi and 5G devices in Canada's 6GHz band.", 
-            "Skills": ["Power Electronics", "UART", "STM32"]},
+            "Skills": []},
 
         "carobot":
             {"Date": "Jun. 2019 - Aug 2019", 
             "Title": "Robotics Instructor", 
             "Company": "Carobot", 
             "Link": "https://learn.carobot.ca/", 
-            "Blurb": "Created lessons and taught kiddos coding and robotics fundamentals (sensors, motors, controllers).", 
-            "Skills": ["Power Electronics", "UART", "STM32"]},
+            "Blurb": "Made lessons and taught kiddos coding and robotics fundamentals.", 
+            "Skills": ["Arduino", "Raspberry Pi", "PCBs", "Sensors", "Motors", "Lego Robotics"]},
 
         "lts":
             {"Date": "Oct. 2015 - Apr. 2018", 
@@ -36,6 +36,6 @@ var jobs = {
             "Company": "City of Toronto Parks and Recreation", 
             "Link": "https://www.toronto.ca/data/parks/prd/skating/reg/learn/index.html", 
             "Blurb": "Taught basic skills & beginner figure skating elements to young & adult skaters.", 
-            "Skills": ["Power Electronics", "UART", "STM32"]}
+            "Skills": []}
 }
 
