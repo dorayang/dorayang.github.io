994044748db1adee3efff20ee62e50196a2eee43
dyang22 <dyang22@student.ubc.ca>
2023-11-19 20:20:34 -0500

    finished first draft of project page

---
+++ projects.js
@@ -0,0 +1,54 @@
+var projects = { 
+    "cave-comms":
+        {"Name": "Cave Rescue Commmunication Module", 
+        "Year": "2022-2023", 
+        "Photo": "images/cave-comms.png", 
+        "Description": "Underwater wireless text messaging system designed for flooded cave conditions.", 
+        "Skills": ["C++", "Flyback Converter", "A2D Converter", "Gate Drivers", "Amplitude Modulation"]},
+    "dyno":
+        {"Name": "Dynomometer", 
+        "Year": "2023", 
+        "Photo": "images/dyno.png", 
+        "Description": "Electronics for a custom motor test bench complete with torque control and performance data collection for UBC Supermileage design team.", 
+        "Skills": ["KiCAD", "Raspberry Pi Pico", "Sensors - Optical, Force", "PID Control"]},
+    "can":
+        {"Name": "CANBus Accessories PCB", 
+        "Year": "2021-2022", 
+        "Photo": "images/canbus.png", 
+        "Description": "Board that houses and controls the vehicle accessories (lights, wipers, horn) for UBC Supermileage's Urban Concept vehicle.", 
+        "Skills": ["KiCAD", "CANBus","STM32 Nucleo", "C++", "Mbed"]},
+
+    "ml-license-plate":
+        {"Name": "Machine Learning Parking Simulation", 
+        "Year": "2021", 
+        "Photo": "images/parking.gif", 
+        "Description": "Self driving and license plate detecting robot designed using machine learning methods and deployed with ROS.", 
+        "Skills": ["Computer Vision", "OpenCV", "Keras", "Reinforcement Learning", "ROS"]},
+    
+    "double-slit":
+        {"Name": "Double Slit Experiment Numerical Simulation", 
+        "Year": "2022", 
+        "Photo": "images/double-slit.gif", 
+        "Description": "Study of finite diffference methods and implicit numerical solutions for the Schr&#776dinger equation", 
+        "Skills": ["MATLAB"]},
+    "recyling-bot":
+        {"Name": "Recycling Robot", 
+        "Year": "2020", 
+        "Photo": "images/robot.png", 
+        "Description": "Line following robot that identifies, collects and dumps aluminum cans.", 
+        "Skills": ["STM32 Blue Pill", "C++", "CAD - Onshape", "Sensors - IR, Ultrasonic", "Motors - DC, Servo"]},
+
+    "ligo":
+        {"Name": "LIGO Coating Mechanical Loss Testing", 
+        "Year": "2021-2022", 
+        "Photo": "images/ligo.png", 
+        "Description": "System to measure the loss associated with different materials used in LIGO's mirrors for gravitational wave detection.", 
+        "Skills": ["COMSOL", "ANSYS - Lumerical", "K-Layout", "Python"]},
+    
+    "interferometer":
+        {"Name": "Michelson Interferometer Experiment", 
+        "Year": "2023", 
+        "Photo": "images/optics.png", 
+        "Description": "Optics experiment investigating light sources and interference pattern behaviour in interferometers.", 
+        "Skills": ["MATLAB", "Optical Tables"]},
+}
