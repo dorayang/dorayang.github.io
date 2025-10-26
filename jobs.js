5ee4f5ad05f43b2de12f4890ec39104ff2d75367
dyang22 <dyang22@student.ubc.ca>
2025-10-26 10:35:04 -0500

    resume refresh

--- jobs.js
+++ jobs.js
@@ -1,41 +1,56 @@
-var jobs = { 
+var jobs = {
+  "sibel-sys":
+      {"Date": "Sep. 2024 - Present",
+      "Title": "Systems Engineer",
+      "Company": "Sibel Health",
+      "Link": "https://sibelhealth.com/",
+      "Blurb": "Medical device systems engineer of wireles vital sign sensors and maternal patient monitors. Authoring/maintaining system requirements, coordinating V&V execution and managing risk management files.",
+      "Skills": ["MatrixALM", "Python", "IEC 60601", "ISO 14791"]},
+
+    "sibel-hw":
+        {"Date": "Jan. 2024 - Sep. 2024",
+        "Title": "Hardware Test Engineer",
+        "Company": "Sibel Health",
+        "Link": "https://sibelhealth.com/",
+        "Blurb": "Supported official verification and engineering testing of wireless vital sign sensors.",
+        "Skills": ["Automation", "Python", "CAD", "PCBA Bring Up", "Power Analyzers", "Oscilloscopes",]},
+
         "dwave":
-            {"Date": "May 2022 - Aug. 2022", 
-            "Title": "Electrical Engineer Co-op", 
-            "Company": "D-Wave", 
-            "Link": "https://www.dwavesys.com/", 
-            "Blurb": "Designed programmable power supply units and test modules for quantum computers.", 
+            {"Date": "May 2022 - Aug. 2022",
+            "Title": "Electrical Engineer Co-op",
+            "Company": "D-Wave",
+            "Link": "https://www.dwavesys.com/",
+            "Blurb": "Designed programmable power supply units and test modules for quantum computers.",
             "Skills": ["Power Electronics", "C++", "Python", "CAD", "3D Printing", "UART", "STM32 Microcontrollers",]},
 
-        "pni": 
-            {"Date": "May 2021 - Dec. 2021", 
-            "Title": "Instrumentation Engineer Co-op", 
-            "Company": "Precision Nanosystems", 
-            "Link": "https://www.precisionnanosystems.com/", 
-            "Blurb": "Created and executed tests to evaluate system performance and guide component selection for a commercial nanomedicine formulation machine.", 
+        "pni":
+            {"Date": "May 2021 - Dec. 2021",
+            "Title": "Instrumentation Engineer Co-op",
+            "Company": "Precision Nanosystems (now part of Cytiva/Danaher)",
+            "Link": "https://www.cytivalifesciences.com/en/pl/solutions/bioprocessing/services/nanomedicine-development-and-manufacturing/lipid-nanoparticle-portfolio",
+            "Blurb": "Created and executed tests to evaluate system performance and guide component selection for a commercial nanomedicine formulation machine.",
             "Skills": ["MATLAB", "Python", "Sensors", "Oscilloscopes", "PLCs", "Data Acquisition", "Labview"]},
         "ised":
-            {"Date": "Jan. 2020 - Apr. 2020", 
-            "Title": "Telecommunications Engineer Co-op", 
-            "Company": "Innovation, Science and Economic Development Canada", 
-            "Link": "https://tinyurl.com/3dzv34hz", 
-            "Blurb": "Contributed to the technical regulations for deploying Wi-Fi and 5G devices in Canada's 6GHz band.", 
-            "Skills": ["Python", "Powerpoint"]},
+            {"Date": "Jan. 2020 - Apr. 2020",
+            "Title": "Telecommunications Engineer Co-op",
+            "Company": "Innovation, Science and Economic Development Canada",
+            "Link": "https://tinyurl.com/3dzv34hz",
+            "Blurb": "Contributed to the technical regulations for deploying Wi-Fi and 5G devices in Canada's 6GHz band.",
+            "Skills": ["Python", "Powerpoint", "EMC Testing"]},
 
         "carobot":
-            {"Date": "Jun. 2019 - Aug 2019", 
-            "Title": "Robotics Instructor", 
-            "Company": "Carobot", 
-            "Link": "https://learn.carobot.ca/", 
-            "Blurb": "Made lessons and taught kiddos coding and robotics fundamentals.", 
+            {"Date": "Jun. 2019 - Aug 2019",
+            "Title": "Robotics Instructor",
+            "Company": "Carobot",
+            "Link": "https://learn.carobot.ca/",
+            "Blurb": "Made lessons and taught kiddos coding and robotics fundamentals.",
             "Skills": ["Arduino", "Raspberry Pi", "PCBs", "Sensors", "Motors", "Lego Robotics"]},
 
         "lts":
-            {"Date": "Oct. 2015 - Apr. 2018", 
-            "Title": "Learn to Skate Instructor", 
-            "Company": "City of Toronto Parks and Recreation", 
-            "Link": "https://www.toronto.ca/data/parks/prd/skating/reg/learn/index.html", 
-            "Blurb": "Taught basic skills & beginner figure skating elements to young & adult skaters.", 
+            {"Date": "Oct. 2015 - Apr. 2018",
+            "Title": "Learn to Skate Instructor",
+            "Company": "City of Toronto Parks and Recreation",
+            "Link": "https://www.toronto.ca/data/parks/prd/skating/reg/learn/index.html",
+            "Blurb": "Taught basic skills & beginner figure skating elements to young & adult skaters.",
             "Skills": []}
 }
-
--- projects.js
+++ projects.js
@@ -1,62 +1,55 @@
-var projects = { 
+var projects = {
     "cave-comms":
-        {"Name": "Cave Rescue Commmunication Module", 
-        "Year": "2022-2023", 
-        "Photo": "images/projects/cave-comms.png", 
-        "Description": "Underwater wireless text messaging system designed for flooded cave conditions.", 
+        {"Name": "Cave Rescue Commmunication Module",
+        "Year": "2022-2023",
+        "Photo": "images/projects/cave-comms.png",
+        "Description": "Underwater wireless text messaging system designed for flooded cave conditions.",
         "Skills": ["C++", "Flyback Converter", "A2D Converter", "Amplitude Modulation"],
         "ModalID": "caveModal"},
     "dyno":
-        {"Name": "Dynomometer", 
-        "Year": "2023", 
-        "Photo": "images/projects/dyno.png", 
-        "Description": "Electronics for a custom motor test bench complete with torque control and performance data collection for UBC Supermileage.", 
+        {"Name": "Dynomometer",
+        "Year": "2023",
+        "Photo": "images/projects/dyno.png",
+        "Description": "Electronics for a custom motor test bench complete with torque control and performance data collection for UBC Supermileage.",
         "Skills": ["KiCAD", "Raspberry Pi Pico", "Sensors - Optical, Force", "PID Control"],
         "ModalID": "dynoModal"},
     "can":
-        {"Name": "CANBus Accessories PCB", 
-        "Year": "2022", 
-        "Photo": "images/projects/canbus.png", 
-        "Description": "Board that houses and controls the vehicle accessories (lights, wipers, horn) for UBC Supermileage's Urban Concept vehicle.", 
+        {"Name": "CANBus Accessories PCB",
+        "Year": "2022",
+        "Photo": "images/projects/canbus.png",
+        "Description": "Board that houses and controls the vehicle accessories (lights, wipers, horn) for UBC Supermileage's Urban Concept vehicle.",
         "Skills": ["KiCAD", "CANBus","STM32 Nucleo", "C++", "Mbed"],
         "ModalID": "canModal"},
-
+      "ligo":
+            {"Name": "LIGO Coating Tester",
+            "Year": "2021-2022",
+            "Photo": "images/projects/ligo.png",
+            "Description": "System to measure the loss associated with different materials used in LIGO's mirrors for gravitational wave detection.",
+            "Skills": ["COMSOL", "ANSYS - Lumerical", "K-Layout", "Python"],
+            "ModalID": "ligoModal"},
     "ml-license-plate":
-        {"Name": "Machine Learning Parking Simulation", 
-        "Year": "2021", 
-        "Photo": "images/projects/parking.gif", 
-        "Description": "Self driving and license plate detecting robot designed using machine learning methods and deployed with ROS.", 
+        {"Name": "Machine Learning Parking Simulation",
+        "Year": "2021",
+        "Photo": "images/projects/parking.gif",
+        "Description": "Self driving and license plate detecting robot designed using machine learning methods and deployed with ROS.",
         "Skills": ["Computer Vision", "OpenCV", "Keras", "Reinforcement Learning", "ROS"],
         "ModalID": "licenseModal"},
-    
+
     "double-slit":
-        {"Name": "Double Slit Experiment Numerical Simulation", 
-        "Year": "2022", 
-        "Photo": "images/projects/double-slit.gif", 
-        "Description": "Study of finite difference methods and implicit numerical solutions for the Schro&#776dinger equation.", 
+        {"Name": "Double Slit Experiment Numerical Simulation",
+        "Year": "2022",
+        "Photo": "images/projects/double-slit.gif",
+        "Description": "Study of finite difference methods and implicit numerical solutions for the Schro&#776dinger equation.",
         "Skills": ["MATLAB"],
         "ModalID": "doubleModal"},
     "recyling-bot":
-        {"Name": "Recycling Robot", 
-        "Year": "2020", 
-        "Photo": "images/projects/robot.png", 
-        "Description": "Line following robot that identifies, collects and dumps aluminum cans.", 
+        {"Name": "Recycling Robot",
+        "Year": "2020",
+        "Photo": "images/projects/robot.png",
+        "Description": "Line following robot that identifies, collects and dumps aluminum cans.",
         "Skills": ["STM32 Blue Pill", "C++", "CAD - Onshape", "Sensors", "Motors"],
         "ModalID": "robotModal"},
 
-    "ligo":
-        {"Name": "LIGO Coating Tester", 
-        "Year": "2021-2022", 
-        "Photo": "images/projects/ligo.png", 
-        "Description": "System to measure the loss associated with different materials used in LIGO's mirrors for gravitational wave detection.", 
-        "Skills": ["COMSOL", "ANSYS - Lumerical", "K-Layout", "Python"],
-        "ModalID": "ligoModal"},
-    
-    "interferometer":
-        {"Name": "Personal Website", 
-        "Year": "2023", 
-        "Photo": "", 
-        "Description": "This website! Very meta woahhhhhh.", 
-        "Skills": ["HTML", "CSS", "Javascript"],
-        "ModalID": "websiteModal"},
+
+
 }
